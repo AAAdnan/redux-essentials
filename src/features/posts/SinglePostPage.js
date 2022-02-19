fefdea83d4ea4cf33064821a600bd77359ecd421
Adnan Ahmed <adnan.ahmed01@gmail.com>
2022-02-18 19:04:58 -0500

    added async logic

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
-
-
+import { selectPostById } from './PostsSlice'
+import { TimeAgo } from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params
 
-    const post = useSelector(state => state.posts.find(post => post.id === postId))
+    const post = useSelector(state => selectPostById(state, postId))
 
     if (!post) {
         return (
@@ -24,6 +24,7 @@ export const SinglePostPage = ({ match }) => {
                 <h2>{post.title}</h2>
                 <p className="post-content">{post.content}</p>
                 <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
                 <Link to={`/editPost/${post.id}`} className="button">
                     Edit Post
               </Link>
