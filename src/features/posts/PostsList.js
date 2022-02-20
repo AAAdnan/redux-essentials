26b95be5a37cc1d42a3dd83d7c2b503cf5680cf1
Adnan Ahmed <adnan.ahmed01@gmail.com>
2022-02-20 12:47:58 -0500

    added reaction button

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { TimeAgo } from './TimeAgo'
 import { Spinner } from '../../components/Spinner'
+import { ReactionButtons } from './ReactionButtons'
+
 
 
 import { PostAuthor } from './PostAuthor'
@@ -10,16 +12,17 @@ import { selectAllPosts, fetchPosts } from './PostsSlice'
 
 const PostExcerpt = ({ post }) => {
     return (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-          <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
+        <article className="post-excerpt" key={post.id}>
+            <h3>{post.title}</h3>
+            <div>
+                <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
+            </div>
+            <p className="post-content">{post.content.substring(0, 100)}</p>
+            <ReactionButtons post={post} />
+            <Link to={`/posts/${post.id}`} className="button muted-button">
+                 View Post
+            </Link>
       </article>
     )
   }
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { selectPostById } from './PostsSlice'
 import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
+
 
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params
@@ -22,12 +24,15 @@ export const SinglePostPage = ({ match }) => {
         <section>
             <article className="post">
                 <h2>{post.title}</h2>
-                <p className="post-content">{post.content}</p>
+                <div>
                 <PostAuthor userId={post.user} />
                 <TimeAgo timestamp={post.date} />
+                </div>
+                <p className="post-content">{post.content}</p>
+                    <ReactionButtons post={post} />
                 <Link to={`/editPost/${post.id}`} className="button">
-                    Edit Post
-              </Link>
+                Edit Post
+                </Link>
             </article>
         </section>
     )
